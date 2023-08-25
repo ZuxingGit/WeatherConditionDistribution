f5eea333cac2fd61d7bc6eac39d147bfb3ee0dcd
ZuxingGit <1245766137@qq.com>
2023-08-25 21:52:25 +0930

    Hello between Server&client

--- src/main/java/com/DS/client/GETClient.java
+++ src/main/java/com/DS/client/GETClient.java
@@ -1,5 +1,23 @@
 package com.DS.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class GETClient {
-    
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost", 4567);
+
+        PrintWriter pr =new PrintWriter(s.getOutputStream());
+        pr.println("hello server");
+        pr.flush();
+
+        InputStreamReader in =new InputStreamReader(s.getInputStream());
+        BufferedReader bf =new BufferedReader(in);
+
+        String str= bf.readLine();
+        System.out.println("server: "+str);
+    }
 }
