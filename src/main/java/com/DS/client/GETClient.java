26559be7da9f6e68cc933d1902cc780ae4582cdd
ZuxingGit <1245766137@qq.com>
2023-08-26 11:01:39 +0930

    client-server chat

--- src/main/java/com/DS/client/GETClient.java
+++ src/main/java/com/DS/client/GETClient.java
@@ -1,23 +1,55 @@
 package com.DS.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class GETClient {
     public static void main(String[] args) throws IOException {
-        Socket s = new Socket("localhost", 4567);
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("hello server");
-        pr.flush();
+        try {
+            socket = new Socket("localhost", 4567);
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
 
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader bf = new BufferedReader(in);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-        String str = bf.readLine();
-        System.out.println("server: " + str);
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String msgToSend = scanner.nextLine();
+
+                bufferedWriter.write(msgToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("Server: " + bufferedReader.readLine());
+
+                if (msgToSend.equalsIgnoreCase("BYE"))
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null)
+                    socket.close();
+                if (inputStreamReader != null)
+                    inputStreamReader.close();
+                if (outputStreamWriter != null)
+                    outputStreamWriter.close();
+                if (bufferedReader != null)
+                    bufferedReader.close();
+                if (bufferedWriter != null)
+                    bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/com/DS/server/AggregationServer.java
+++ src/main/java/com/DS/server/AggregationServer.java
@@ -1,27 +1,51 @@
 package com.DS.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class AggregationServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(4567);
-        Socket s = ss.accept();
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket ss = null;
 
-        System.out.println("client connected!");
+        ss = new ServerSocket(4567);
+        while (true) {
+            try {
+                socket = ss.accept();
 
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader bf = new BufferedReader(in);
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
 
-        String str = bf.readLine();
-        System.out.println("client: " + str);
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("hello client");
-        pr.flush();
+                
+                while (true) {
+                    String msgFromClient = bufferedReader.readLine();
+                    System.out.println("Client: " + msgFromClient);
+                  
+                    bufferedWriter.write("MSG Received.");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if (msgFromClient.equalsIgnoreCase("BYE"))
+                        break;
+                }
+
+                socket.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
